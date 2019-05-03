f47e137503a43996c53a4360b1b3d4f3c96faa63
Ryan Barclay <mrryanbarclay@gmail.com>
2019-05-03 09:35:11 -0700

    Day-3
    Was just a simple for loop with a element count

---
+++ Day-3/Day-3.js
@@ -0,0 +1,40 @@
+// DAY 3
+
+/* Description
+--------------------------------------------------------------------------------
+"QUACK QUACK QUACK QUACK QUACK QUACK QUACK", goes LARRY, his eyes still glowing red. He’s counting off each of the seven modules on the list - you must be on the right track! Now to find out how many of them are essential.
+
+Use your JavaScript skills to write a function called countEssential() which will count how many modules from the availableModules array have the essential flag set.
+*/
+
+function powerOn() {
+  /*
+  Description:
+    Changes `powerOn` property of object `ship` to  true.
+  */
+  ship.powerOn = `true`;
+}
+
+function countModules() {
+  /*
+  Description:
+    Counts the amount of modules thare are to choose from
+  */
+  amnt = availableModules.length;
+  return amnt;
+}
+
+function countEssential() {
+  /*
+  Description:
+    Counts the amount of modules from the the avaiableModules that have the essential flag set.
+  */
+  let curCount = 0;
+  for (var i = 0; i < availableModules.length; i++) {
+    let token = availableModules[i];
+    if (token.essential) {
+      curCount++;
+    }
+  } // end of for
+  return curCount;
+}
