4141b09a4393c2efb44d50ed815ce8678d30f453
Ryan Barclay <mrryanbarclay@gmail.com>
2019-05-07 00:12:57 -0700

    Day-7

---
+++ Day-7/Day-7.js
@@ -0,0 +1,84 @@
+// DAY 7
+
+/* Description
+--------------------------------------------------------------------------------
+You look over at LARRY, expecting him to quack out his next prompt, but it seems LARRY is now the one malfunctioning! You open up the manual, and find the section on LARRY. Apparently, loading modules can sometimes cause LARRY to get stuck in an infinite loop.
+
+You can fix him, you'll just need to write some code! Write and call a function called resetLARRY() which will prompt LARRY to quack ten times so he breaks out of his loop.
+
+There is a function called LARRY.quack() that you can use in your code.
+*/
+
+function countModules() {
+  /*
+  Description:
+    Counts the amount of modules thare are to choose from
+  */
+  amnt = availableModules.length;
+  return amnt;
+}
+
+function countEssential() {
+  /*
+  Description:
+    Counts the amount of modules from the the avaiableModules that have the essential flag set.
+  */
+  let curCount = 0;
+  for (var i = 0; i < availableModules.length; i++) {
+    let token = availableModules[i];
+    if (token.essential) {
+      curCount++;
+    }
+  } // end of for
+  return curCount;
+}
+
+function loadModule(index) {
+  /*
+  Description:
+    When loadModule gets the index number of a module, it should load it into the ship's modules property (which is already an array). Before you load it in, set the enabled property to true.
+  */
+  //1.enable on
+  //2.insert into ship.modules
+  availableModules[index].enabled = true;
+  ship.modules.push(availableModules[index]);
+}
+
+function findModuleIndex(targetName) {
+  /*
+  Description:
+    Loops over availableModules and tries to match `targetName` with the property `name` of each module.
+
+    Will return -1 if not in array
+    Does not account for duplicates.
+  */
+  for (var i = 0; i < availableModules.length; i++) {
+    // let token = availableModules[i]
+    if (availableModules[i].name == targetName) {
+      // if (availableModules[i].essential){
+        return i;
+      // }
+    }
+  } //end of for
+  return -1;
+}
+
+function resetLARRY() {
+  for (var i = 0; i < 10; i++) {
+    LARRY.quack();
+  }
+}
+
+
+
+// MAIN
+let string = `life-support`;
+loadModule(findModuleIndex(string));
+
+string = `propulsion`;
+loadModule(findModuleIndex(string));
+
+string = `navigation`;
+loadModule(findModuleIndex(string));
+
+resetLARRY();
