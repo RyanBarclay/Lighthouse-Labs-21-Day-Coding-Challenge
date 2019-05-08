c2b8aaffbbee6f5c0109a398c101113def087bd4
Ryan Barclay <mrryanbarclay@gmail.com>
2019-05-08 00:03:21 -0700

    Day-8

---
+++ Day-8/Day-8.js
@@ -0,0 +1,85 @@
+// DAY 8
+
+/* Description
+--------------------------------------------------------------------------------
+The little duck-shaped robot shudders back and forth for a moment, and his eyes flash as he releases a barrage of ten quacks. "Thank you!" he exclaims.
+
+"COMMUNICATION MODULE NEEDED," LARRY blares suddenly. He repeats it twice more - he’s rather insistent! Luckily, you've got the code for this. Load the module called "communication" using findModuleIndex() and loadModule() from before.
+*/
+
+function countModules() {
+  /*
+  Description:
+    Counts the amount of modules thare are to choose from
+  */
+  amnt = availableModules.length;
+  return amnt;
+}
+
+function countEssential() {
+  /*
+  Description:
+    Counts the amount of modules from the the avaiableModules that have the essential flag set.
+  */
+  let curCount = 0;
+  for (var i = 0; i < availableModules.length; i++) {
+    let token = availableModules[i];
+    if (token.essential) {
+      curCount++;
+    }
+  } // end of for
+  return curCount;
+}
+
+function loadModule(index) {
+  /*
+  Description:
+    When loadModule gets the index number of a module, it should load it into the ship's modules property (which is already an array). Before you load it in, set the enabled property to true.
+  */
+  //1.enable on
+  //2.insert into ship.modules
+  availableModules[index].enabled = true;
+  ship.modules.push(availableModules[index]);
+}
+
+function findModuleIndex(targetName) {
+  /*
+  Description:
+    Loops over availableModules and tries to match `targetName` with the property `name` of each module.
+
+    Will return -1 if not in array
+    Does not account for duplicates.
+  */
+  for (var i = 0; i < availableModules.length; i++) {
+    // let token = availableModules[i]
+    if (availableModules[i].name == targetName) {
+      // if (availableModules[i].essential){
+        return i;
+      // }
+    }
+  } //end of for
+  return -1;
+}
+
+function resetLARRY() {
+  for (var i = 0; i < 10; i++) {
+    LARRY.quack();
+  }
+}
+
+
+
+// MAIN
+let string = `life-support`;
+loadModule(findModuleIndex(string));
+
+string = `propulsion`;
+loadModule(findModuleIndex(string));
+
+string = `navigation`;
+loadModule(findModuleIndex(string));
+
+resetLARRY();
+
+string = `communication`;
+loadModule(findModuleIndex(string));
