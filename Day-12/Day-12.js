9fa93a3c7507d8e9d900e25187eceb79acda2ff5
Ryan Barclay <mrryanbarclay@gmail.com>
2019-05-12 01:02:42 -0700

    Day-12

---
+++ Day-12/Day-12.js
@@ -0,0 +1,124 @@
+// DAY 12
+
+/* Description
+--------------------------------------------------------------------------------
+"QUACK! Navigation system offline!" blares LARRY. "Initialization sequence broken!"
+
+You quickly check the documentation for the navigation system and find out that the nav system's initialization sequence needs three values, because you are in three-dimensional space. It needs an X, Y, and Z value.
+
+Your next task is to write a function called initialize() which will set the x, y, and z values correctly to start off at 0 in the navigation system. For example, the navigation system's x value is navigation.x.
+
+Don't worry about calling your initialize() function, this one runs automatically
+*/
+
+function countModules() {
+  /*
+  Description:
+    Counts the amount of modules thare are to choose from
+  */
+  amnt = availableModules.length;
+  return amnt;
+}
+
+function countEssential() {
+  /*
+  Description:
+    Counts the amount of modules from the the avaiableModules that have the essential flag set.
+  */
+  let curCount = 0;
+  for (var i = 0; i < availableModules.length; i++) {
+    let token = availableModules[i];
+    if (token.essential) {
+      curCount++;
+    }
+  } // end of for
+  return curCount;
+}
+
+function loadModule(index) {
+  /*
+  Description:
+    When loadModule gets the index number of a module, it should load it into the ship's modules property (which is already an array). Before you load it in, set the enabled property to true.
+  */
+  //1.enable on
+  //2.insert into ship.modules
+  availableModules[index].enabled = true;
+  ship.modules.push(availableModules[index]);
+}
+
+function findModuleIndex(targetName) {
+  /*
+  Description:
+    Loops over availableModules and tries to match `targetName` with the property `name` of each module.
+
+    Will return -1 if not in array
+    Does not account for duplicates.
+  */
+  for (var i = 0; i < availableModules.length; i++) {
+    // let token = availableModules[i]
+    if (availableModules[i].name == targetName) {
+      // if (availableModules[i].essential){
+        return i;
+      // }
+    }
+  } //end of for
+  return -1;
+}
+
+function resetLARRY() {
+  /*
+  Description:
+    `resets` Larry by making him quack 10 times???
+    (Kinda lame)
+  */
+  for (var i = 0; i < 10; i++) {
+    LARRY.quack();
+  }
+}
+
+function setMessage(object) {
+  /*
+  Description:
+    Takes an object and sets the message property on the radio object to be the JSON version of the object...
+  */
+  let message = JSON.stringify(object);
+  radio.message = message;
+}
+
+function activateBeacon() {
+  radio.beacon = true;
+}
+
+function setFrequency() {
+  let  highFreq = radio.range;
+  highFreq = highFreq.high;
+  let lowFreq = radio.range;
+  lowFreq = lowFreq.low
+  let val = (highFreq+lowFreq) / 2
+  radio.frequency = val;
+}
+
+function initialize() {
+  navigation.x = 0;
+  navigation.y = 0;
+  navigation.z = 0;
+}
+
+// MAIN
+let string = `life-support`;
+loadModule(findModuleIndex(string));
+
+string = `propulsion`;
+loadModule(findModuleIndex(string));
+
+string = `navigation`;
+loadModule(findModuleIndex(string));
+
+resetLARRY();
+
+string = `communication`;
+loadModule(findModuleIndex(string));
+
+setMessage(navigation);
+
+activateBeacon();
